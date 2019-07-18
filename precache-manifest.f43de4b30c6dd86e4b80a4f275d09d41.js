3904a9e27e62a133313b715a7dfce80f7aa7fb34
K Hulett <kblocent@gmail.com>
2019-07-18 15:01:46 -0400

    Updates

---
+++ precache-manifest.f43de4b30c6dd86e4b80a4f275d09d41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc5710c9863c29cad39872739645970",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "567811fb78f1d41f0a5f",
+    "url": "/todolist/static/css/main.f8299b5b.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/todolist/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "567811fb78f1d41f0a5f",
+    "url": "/todolist/static/js/main.444ce106.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
